ff2a1ba2d3aa05d20df22b8b14038725747f232d
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-04-23 16:01:13 +0530

    fixed files format

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -7,7 +7,7 @@ export class HomePage {
         this.searchInput = page.locator('input[name=q]');
     }
 
-    
+
 
     async open() {
         await this.page.goto('/');
@@ -15,12 +15,12 @@ export class HomePage {
     }
 
     async verifyTitle() {
-        const title=await this.page.title();
-         expect(title).toBe('Google');
+        const title = await this.page.title();
+        expect(title).toBe('Google');
 
     }
-    async search(text:string) {
+    async search(text: string) {
         await this.searchInput.fill(text);
         await this.searchInput.press('Enter');
-      }
+    }
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,20 +1,20 @@
 import { test, Page } from '@playwright/test';
-import {HomePage} from '../pages/home-page';
+import { HomePage } from '../pages/home-page';
 
 test.describe('Google home page tests', () => {
-    
+
     test('Navigate to home page', async ({ page }) => {
-      
-        const homePage=new HomePage(page);
+
+        const homePage = new HomePage(page);
         await homePage.open();
         await homePage.verifyTitle();
     });
 
     test('Search on google', async ({ page }) => {
-      
-        const homePage=new HomePage(page);
+
+        const homePage = new HomePage(page);
         await homePage.open();
         await homePage.search('Vikash Chauhan on github');
     });
-    
+
 })
